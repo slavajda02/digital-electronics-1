2dbe231ee3e94924e746b7057fd6fa981d8a18e9
slavajda02 <slavajda@gmail.com>
2023-02-22 12:05:11 +0100

    added comparator_project

---
+++ 03-vivado/Comparator/Comparator.srcs/sim_1/new/tb_comparator_4bit.vhd
@@ -0,0 +1,68 @@
+------------------------------------------------------------
+--
+-- Testbench for 4-bit binary comparator.
+-- EDA Playground
+--
+-- Copyright (c) 2023 Tomas Fryza and Miloslav Kužela
+-- Dept. of Radio Electronics, Brno Univ. of Technology, Czechia
+-- This work is licensed under the terms of the MIT license.
+--
+------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+------------------------------------------------------------
+-- Entity declaration for testbench
+------------------------------------------------------------
+entity tb_comparator_4bit is
+    -- Entity of testbench is always empty
+end entity tb_comparator_4bit;
+
+------------------------------------------------------------
+-- Architecture body for testbench
+------------------------------------------------------------
+architecture testbench of tb_comparator_4bit is
+
+    -- Local signals
+    signal s_a           : std_logic_vector(4 - 1 downto 0);
+    signal s_b           : std_logic_vector(4 - 1 downto 0);
+    signal s_B_greater_A : std_logic;
+    signal s_B_equals_A  : std_logic;
+    signal s_B_less_A    : std_logic;
+
+begin
+    -- Connecting testbench signals with comparator_4bit
+    -- entity (Unit Under Test)
+    uut_comparator_4bit : entity work.comparator_4bit
+        port map(
+            a_i           => s_a,
+            b_i           => s_b,
+            B_greater_A_o => s_B_greater_A,
+            B_equals_A_o  => s_B_equals_A,
+            B_less_A_o    => s_B_less_A
+        );
+
+    --------------------------------------------------------
+    -- Data generation process
+    --------------------------------------------------------
+    p_stimulus : process
+    begin
+        -- Report a note at the beginning of stimulus process
+        report "Stimulus process started";
+        
+        s_b <= "0100"; -- Such as "0101" if ID = xxxx56
+        s_a <= "1000";        -- Such as "0110" if ID = xxxx56
+        wait for 100 ns;
+        -- Expected output
+        assert ((s_B_greater_A = '0') and
+                (s_B_equals_A  = '0') and
+                (s_B_less_A    = '1'))
+        -- If false, then report an error
+        report "Input combination b=0100, a=1000" severity error;
+		report "ID task process finished";
+		
+        wait; -- Data generation process is suspended forever
+    end process p_stimulus;
+
+end architecture testbench;
