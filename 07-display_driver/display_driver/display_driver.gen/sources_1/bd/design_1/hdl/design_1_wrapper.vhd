932c368f0304d7aedacdef680cbcfe559e6d96a7
slavajda02 <slavajda@gmail.com>
2023-03-27 18:15:04 +0200

    added picture

---
+++ 07-display_driver/display_driver/display_driver.gen/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
@@ -0,0 +1,23 @@
+--Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2022.2 (lin64) Build 3671981 Fri Oct 14 04:59:54 MDT 2022
+--Date        : Sun Mar 26 23:38:38 2023
+--Host        : argon-linux running 64-bit unknown
+--Command     : generate_target design_1_wrapper.bd
+--Design      : design_1_wrapper
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity design_1_wrapper is
+end design_1_wrapper;
+
+architecture STRUCTURE of design_1_wrapper is
+  component design_1 is
+  end component design_1;
+begin
+design_1_i: component design_1
+ ;
+end STRUCTURE;
---
+++ 07-display_driver/display_driver/display_driver.gen/sources_1/bd/design_1/synth/design_1.vhd
@@ -0,0 +1,9 @@
+--Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2022.2 (lin64) Build 3671981 Fri Oct 14 04:59:54 MDT 2022
+--Date        : Sun Mar 26 23:38:38 2023
+--Host        : argon-linux running 64-bit unknown
+--Command     : generate_target design_1.bd
+--Design      : design_1
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
